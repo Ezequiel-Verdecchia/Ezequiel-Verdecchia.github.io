36aff4180be0e7ae3f2d1ee041bc509760ddc073
Ezequiel <89552216+Ezequiel-Verdecchia@users.noreply.github.com>
2024-01-05 21:52:47 +0000

    F

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ ScrollReveal().reveal('.home-content p, .about-content', {origin:'right'});
 
 //typed js
 const typed = new Typed('.multiple-text',{
-     strings:['python','sql','HTML','CSS','JAVASCRIPT'],
+     strings:['Python','sql','HTML','CSS','JAVASCRIPT'],
      typeSpeed: 100,
      backSpeed: 100,
      backDelay: 1000,
